24fab2efc25c942e44d9a82ce12dadde80a9a858
노영오 <nohyo05@naver.com>
2025-07-05 10:46:59 +0900

    Init: Add zerofive_user module

---
+++ src/main/java/org/example/zerofive_user/ZerofiveUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.zerofive_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerofiveUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerofiveUserApplication.class, args);
+    }
+
+}
