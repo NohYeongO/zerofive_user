e81c0c3bdda3c6bb0121994a77ad4d24c2fd6308
노영오 <nohyo05@naver.com>
2025-07-06 08:41:36 +0900

    feat: Spring SecurityConfig 설정 및 커스텀 Exception 추가 - PasswordEncoder Bean 등록 및 임시 FilterChain 구현 - UserException 커스텀 Exception 추가

---
+++ src/main/java/com/zerofive/user/common/exception/UserException.java
@@ -0,0 +1,14 @@
+package com.zerofive.user.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UserException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public UserException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
