167cd41db11f3007e2b2021457a27cd028ca37ed
노영오 <nohyo05@naver.com>
2025-07-05 17:37:27 +0900

    Init: 프로젝트 초기 세팅

--- src/main/java/com/zerofive/user/ZerofiveUserApplication.java
+++ src/main/java/com/zerofive/user/ZerofiveUserApplication.java
@@ -1,13 +1,11 @@
-package org.example.zerofive_user;
+package com.zerofive.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZerofiveUserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZerofiveUserApplication.class, args);
     }
-
 }
